3fe43f58fd3319bcd491ebed7e85d315a3f423a8
IJakovic <ijakov1@racunarstvo.hr>
2022-07-08 18:51:38 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 $(document).ready(function () {
 
-    //Loading components
+    //Load components
     $("#header").load("components/navbar.html"); 
     $("#footer").load("components/footer.html"); 
 
-    //Lightbox
-    Fancybox.bind('[data-fancybox="gallery"]', {
-      infinite: true
-    });
+    //Fancybox - image gallery
+    if (document.URL.includes("news1.html") ) {
+      Fancybox.bind('[data-fancybox="gallery"]', {
+        infinite: true
+      });
+    }
 
     //Curriculum
+    let getAllCurriculum = $.ajax({
+      type: "GET",
+      url: "http://www.fulek.com/VUA/SUPIT/GetNastavniPlan",
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+      }
+    });
 
+    $("#curriculumSearch").autocomplete({
+      minLength: 0,
+      source: getAllCurriculum,
+      select: function(event, ui) { 
+          $("#curriculumSearch").val(ui.item.label);
+      }
+    });
 });
